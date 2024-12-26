31fdaf26103bdf92c30835e5d87d50529831a03d
AyushGaurNomo <ayush.gaur@nomo.tech>
2024-12-26 19:51:41 +0530

    Wrote a failing test for expand() method.

--- tests/url-shortner.test.js
+++ tests/url-shortner.test.js
@@ -1,10 +1,22 @@
 const UrlShortener = require('../src/url-shortner');
 
 describe('UrlShortener', () => {
+    let shortener;
+
+    beforeEach(() => {
+        shortener = new UrlShortener();
+    });
+
     it('should generate a short URL for a given long URL', () => {
-        const shortener = new UrlShortener();
         const longUrl = 'https://example.com/some/long/path';
         const shortUrl = shortener.shorten(longUrl);
         expect(shortUrl).toMatch(/^http:\/\/short.ly\/[a-zA-Z0-9]{6}$/);
+    });
+
+    it('should retrieve the original URL for a given short URL', () => {
+        const longUrl = 'https://example.com/some/long/path';
+        const shortUrl = shortener.shorten(longUrl);
+        const retrievedUrl = shortener.expand(shortUrl);
+        expect(retrievedUrl).toBe(longUrl);
     });
 });
